6f80dfbc0d08e585d91f40c4afb6f1131837a631
zaza27 <jean_exavier@yahoo.com>
2018-10-31 23:31:05 -0500

    Deleted

---
+++ twitter_credentials.py
@@ -0,0 +1,6 @@
+# Variables that contain the user credentials to access Twitter API.
+
+ACCESS_TOKEN = "106283509-knvKYaEkvF3IPya7T8YP85HSxzCRtedOfEXgI3iS"
+ACCESS_TOKEN_SECRET = "2BjZfWNUH3fgsDwmhJSJRIv1mmD1nWmaTv11ZfodUpJcm"
+CONSUMER_KEY = "5JEZ14G8yEE6A0UtBH6cytTcU"
+CONSUMER_SECRET = "o1xoaJkNtjW2qPq2MfsO3Lq8bH8gX3HLpL0gfWapbgyR1JQ52b"
